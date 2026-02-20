8200c4f736aa1cff5f1401a2aae3897015057a6b
sabilulbari <mdsabilulbari100@gmail.com>
2026-02-20 20:54:59 +0600

    home bug

--- script/login.js
+++ script/login.js
@@ -1,17 +1,14 @@
-document.getElementById("btn-login")
-.addEventListener("click", function (event) {
+document.getElementById("btn-login").addEventListener("click", function (event) {
   const mobileNumber = document.getElementById("mobile-num");
   const number = mobileNumber.value;
 
   const pinNumber = document.getElementById("pin-number");
   const pin = pinNumber.value;
 
-  if(number === "01234567890" && pin === "1234"){
-    alert("Log in Success")
-    window.location.assign("/home.html");
-  }
-
-  else{
+  if (number === "01234567890" && pin === "1234") {
+    alert("Log in Success");
+    window.location.assign("cashout.html");
+  } else {
     alert("Login failed");
     event.number = "";
     event.pin = "";
